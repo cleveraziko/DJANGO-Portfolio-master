9ce38e9cd8f6c378e15f8a8b189f1a75d00fdeb1
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-11-01 01:47:59 +0500

    ready

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,6 +2,7 @@ from decouple import config
 # import dj_database_url
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -169,3 +170,5 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
